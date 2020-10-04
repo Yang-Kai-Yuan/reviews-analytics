d34fd56508485280765c442fe121e97a32e10ee6
yang-kai-yuan <haha200ha@gmail.com>
2020-10-04 23:06:18 +0800

    reviews-analytics-len100

--- read3.py
+++ read3.py
@@ -11,4 +11,10 @@ print('總共有', len(data), '則留言')
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
-print('留言平均是', sum_len/len(data), '個字')
+print('留言平均是', sum_len/len(data), '個字母')
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('總共有', len(new), '則留言長度小於100個字母')
